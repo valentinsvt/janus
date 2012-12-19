01530757fdffb140b7775dae8c791cec16cb51ab
valentinsvt <valentinsvt@hotmail.com>
2012-12-19 11:32:57 -0500

    println debuging

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -29,10 +29,10 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:postgresql://10.0.0.3:5432/janus2"
+            url = "jdbc:postgresql://127.0.0.1:5432/gadpp"
             username = "postgres"
-            password = "postgres"
-//            password = "postgres"
+            password = "janus"
         }
     }
+
 }
