3902e26e2c6e7c02dc33847b9c87bb0a53eedfbe
luzma87 <luzma_87@yahoo.com>
2013-01-14 10:44:43 -0500

    concursos generate all

---
+++ grails-app/controllers/janus/ContratoController.groovy
@@ -0,0 +1,107 @@
+package janus
+
+import org.springframework.dao.DataIntegrityViolationException
+
+class ContratoController extends janus.seguridad.Shield {
+
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
+    def index() {
+        redirect(action: "list", params: params)
+    } //index
+
+    def list() {
+        [contratoInstanceList: Contrato.list(params), params: params]
+    } //list
+
+    def form_ajax() {
+        def contratoInstance = new Contrato(params)
+        if (params.id) {
+            contratoInstance = Contrato.get(params.id)
+            if (!contratoInstance) {
+                flash.clase = "alert-error"
+                flash.message = "No se encontró Contrato con id " + params.id
+                redirect(action: "list")
+                return
+            } //no existe el objeto
+        } //es edit
+        return [contratoInstance: contratoInstance]
+    } //form_ajax
+
+    def save() {
+        def contratoInstance
+        if (params.id) {
+            contratoInstance = Contrato.get(params.id)
+            if (!contratoInstance) {
+                flash.clase = "alert-error"
+                flash.message = "No se encontró Contrato con id " + params.id
+                redirect(action: 'list')
+                return
+            }//no existe el objeto
+            contratoInstance.properties = params
+        }//es edit
+        else {
+            contratoInstance = new Contrato(params)
+        } //es create
+        if (!contratoInstance.save(flush: true)) {
+            flash.clase = "alert-error"
+            def str = "<h4>No se pudo guardar Contrato " + (contratoInstance.id ? contratoInstance.id : "") + "</h4>"
+
+            str += "<ul>"
+            contratoInstance.errors.allErrors.each { err ->
+                def msg = err.defaultMessage
+                err.arguments.eachWithIndex { arg, i ->
+                    msg = msg.replaceAll("\\{" + i + "}", arg.toString())
+                }
+                str += "<li>" + msg + "</li>"
+            }
+            str += "</ul>"
+
+            flash.message = str
+            redirect(action: 'list')
+            return
+        }
+
+        if (params.id) {
+            flash.clase = "alert-success"
+            flash.message = "Se ha actualizado correctamente Contrato " + contratoInstance.id
+        } else {
+            flash.clase = "alert-success"
+            flash.message = "Se ha creado correctamente Contrato " + contratoInstance.id
+        }
+        redirect(action: 'list')
+    } //save
+
+    def show_ajax() {
+        def contratoInstance = Contrato.get(params.id)
+        if (!contratoInstance) {
+            flash.clase = "alert-error"
+            flash.message = "No se encontró Contrato con id " + params.id
+            redirect(action: "list")
+            return
+        }
+        [contratoInstance: contratoInstance]
+    } //show
+
+    def delete() {
+        def contratoInstance = Contrato.get(params.id)
+        if (!contratoInstance) {
+            flash.clase = "alert-error"
+            flash.message = "No se encontró Contrato con id " + params.id
+            redirect(action: "list")
+            return
+        }
+
+        try {
+            contratoInstance.delete(flush: true)
+            flash.clase = "alert-success"
+            flash.message = "Se ha eliminado correctamente Contrato " + contratoInstance.id
+            redirect(action: "list")
+        }
+        catch (DataIntegrityViolationException e) {
+            flash.clase = "alert-error"
+            flash.message = "No se pudo eliminar Contrato " + (contratoInstance.id ? contratoInstance.id : "")
+            redirect(action: "list")
+        }
+    } //delete
+} //fin controller
--- grails-app/domain/janus/Contrato.groovy
+++ grails-app/domain/janus/Contrato.groovy
@@ -1,7 +1,34 @@
 package janus
 
+import janus.pac.Oferta
+import janus.pac.PeriodoValidez
+import janus.pac.TipoContrato
+import janus.pac.TipoPlazo
+
 class Contrato implements Serializable {
 
+    Oferta oferta
+    TipoContrato tipoContrato
+    TipoPlazo tipoPlazo
+    Contrato padre
+    PeriodoValidez periodoValidez
+    String codigo
+    String objeto
+    Date fechaSubscripcion
+    Date fechaIngreso
+    Date fechaInicio
+    Date fechaFin
+    Double monto
+    Double financiamiento
+    Double anticipo
+    Double multas
+    String estado
+    String responsableTecnico
+    Date fechaFirma
+    String cuentaContable
+    String prorroga
+    String observaciones
+    String memo
 
     static mapping = {
 
@@ -13,10 +40,54 @@ class Contrato implements Serializable {
         columns {
             id column: 'cntr__id'
 
+            oferta column: 'ofrt__id'
+            tipoContrato column: 'tpcr__id'
+            tipoPlazo column: 'tppz__id'
+            padre column: 'cntrpdre'
+            periodoValidez column: 'prin__id'
+            codigo column: 'cntrcdgo'
+            objeto column: 'cntrobjt'
+            fechaSubscripcion column: 'cntrfcsb'
+            fechaIngreso column: 'cntrfcig'
+            fechaInicio column: 'cntrfcin'
+            fechaFin column: 'cntrfcfn'
+            monto column: 'cntrmnto'
+            financiamiento column: 'cntrfina'
+            anticipo column: 'cntrpcan'
+            multas column: 'cntrmlta'
+            estado column: 'cntretdo'
+            responsableTecnico column: 'cntrrptc'
+            fechaFirma column: 'cntrfcfr'
+            cuentaContable column: 'cntrcnta'
+            prorroga column: 'cntrprrg'
+            observaciones column: 'cntrobsr'
+            memo column: 'cntrmemo'
+
         }
     }
 
     static constraints = {
-
+        oferta(blank: false, nullable: false)
+        tipoContrato(blank: false, nullable: false)
+        tipoPlazo(blank: false, nullable: false)
+        padre(blank: false, nullable: false)
+        periodoValidez(blank: false, nullable: false)
+        codigo(blank: false, nullable: false)
+        objeto(blank: false, nullable: false)
+        fechaSubscripcion(blank: false, nullable: false)
+        fechaIngreso(blank: false, nullable: false)
+        fechaInicio(blank: false, nullable: false)
+        fechaFin(blank: false, nullable: false)
+        monto(blank: false, nullable: false)
+        financiamiento(blank: false, nullable: false)
+        anticipo(blank: false, nullable: false)
+        multas(blank: false, nullable: false)
+        estado(blank: false, nullable: false)
+        responsableTecnico(blank: false, nullable: false)
+        fechaFirma(blank: false, nullable: false)
+        cuentaContable(blank: false, nullable: false)
+        prorroga(blank: false, nullable: false)
+        observaciones(blank: false, nullable: false)
+        memo(blank: false, nullable: false)
     }
 }
