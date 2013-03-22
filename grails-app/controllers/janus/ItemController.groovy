c4f2b9a5819f32fd564d45b7d34ddecd8161c8a5
guido8a <guido8a@gmail.com>
2013-03-22 14:49:02 -0500

    version 0.6.12 -- cambios en listas de precios

--- grails-app/controllers/janus/ItemController.groovy
+++ grails-app/controllers/janus/ItemController.groovy
@@ -132,8 +132,7 @@ class ItemController extends janus.seguridad.Shield {
                 itemsIds += row[0]
             }
 //
-//            println itemsIds
-
+//          println itemsIds
             if (itemsIds == "") itemsIds = '-1'
 
             def precios = preciosService.getPrecioRubroItemEstado(f, lugar, itemsIds, estado)
