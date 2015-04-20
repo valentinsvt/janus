e3256361d561faa2f58d596928f9139fece941fe
fabricio <fegrijalva2501@hotmail.com>
2015-04-20 15:53:42 -0500

    arreglo observacones de planilla

--- grails-app/controllers/janus/ejecucion/ReportePlanillas3Controller.groovy
+++ grails-app/controllers/janus/ejecucion/ReportePlanillas3Controller.groovy
@@ -197,7 +197,12 @@ class ReportePlanillas3Controller {
                 tablaFirmas.setWidths(arregloEnteros([35, 30, 35]))
 
 //                addCellTabla(tablaFirmas, new Paragraph("Observaciones: " + planilla?.observaciones, fontThFirmas), [border: Color.BLACK, bg: Color.LIGHT_GRAY, align: Element.ALIGN_LEFT, valign: Element.ALIGN_MIDDLE, colspan: 8])
-                addCellTabla(tablaFirmas, new Paragraph("Observaciones: " + planilla?.observaciones, fontThFirmas), [border: Color.WHITE, bg: Color.WHITE, align: Element.ALIGN_LEFT, valign: Element.ALIGN_MIDDLE, colspan: 8])
+
+                if(planilla?.observaciones != null){
+                    addCellTabla(tablaFirmas, new Paragraph("Observaciones: " + planilla?.observaciones, fontThFirmas), [border: Color.WHITE, bg: Color.WHITE, align: Element.ALIGN_LEFT, valign: Element.ALIGN_MIDDLE, colspan: 8])
+                }else{
+                    addCellTabla(tablaFirmas, new Paragraph("Observaciones: ", fontThFirmas), [border: Color.WHITE, bg: Color.WHITE, align: Element.ALIGN_LEFT, valign: Element.ALIGN_MIDDLE, colspan: 8])
+                }
 
                 addCellTabla(tablaFirmas, new Paragraph("", fontThFirmas), [height: 35, bwb: 1, bcb: Color.BLACK, border: Color.WHITE, align: Element.ALIGN_CENTER, valign: Element.ALIGN_MIDDLE])
                 addCellTabla(tablaFirmas, new Paragraph("", fontThFirmas), [height: 35, border: Color.WHITE, align: Element.ALIGN_CENTER, valign: Element.ALIGN_MIDDLE])
