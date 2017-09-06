db6058cd0523a5acbf37f9aadbfb57b29473a057
guido8a <guido8a@gmail.com>
2017-09-06 15:54:05 -0500

    Informe de planillas

--- grails-app/controllers/janus/ReportesPlanillasController.groovy
+++ grails-app/controllers/janus/ReportesPlanillasController.groovy
@@ -686,7 +686,9 @@ class ReportesPlanillasController {
 //        def multas = planillasAvance.sum { it.multaRetraso + it.multaPlanilla } ?: 0
         def tx = "select sum(mlplmnto) suma from mlpl where plnl__id in (select plnl__id from plnl " +
                 "where cntr__id = ${plnl.contrato.id} and tppl__id in (3,9,4) and plnlfcfn <= '${plnl.fechaFin}')"
-        def multas = cn.rows(tx.toString())[0].suma
+
+        def multas = cn.rows(tx.toString())[0].suma + plnl.multaEspecial?:0
+
 //        println "sum: $tx"
         def baos = new ByteArrayOutputStream()
         def name = "avance_" + new Date().format("ddMMyyyy_hhmm") + ".pdf";
