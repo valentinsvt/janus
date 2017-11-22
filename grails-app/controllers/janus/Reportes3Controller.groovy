c098643674d5706462039895365154c65a7d2930
fabricio4077 <orejona2501>
2017-11-22 12:49:26 -0500

    reportes de garantias

--- grails-app/controllers/janus/Reportes3Controller.groovy
+++ grails-app/controllers/janus/Reportes3Controller.groovy
@@ -2955,7 +2955,7 @@ class Reportes3Controller {
 //        }
 
 
-        def garantias = Garantia.findAllByContratoInListAndEstado(filtrados, estado)
+        def garantias = Garantia.findAllByContratoInListAndEstado(filtrados, estado, [sort:"contrato.contratista.nombre",order:'desc'],[sort:"fechaInicio",order:'desc'])
 
 
         def auxiliar = Auxiliar.get(1)
@@ -3089,7 +3089,7 @@ class Reportes3Controller {
 //        }
 
 
-        def garantias = Garantia.findAllByContratoInListAndFechaFinalizacionLessThanAndEstadoNotEqualAndEstadoNotEqual(filtrados, hoy, estadoRev, estadoDev)
+        def garantias = Garantia.findAllByContratoInListAndFechaFinalizacionLessThanAndEstadoNotEqualAndEstadoNotEqual(filtrados, hoy, estadoRev, estadoDev, [sort:"contrato.contratista.nombre",order:'desc'],[sort:"fechaInicio",order:'desc'])
 
 //        println("garantias " + garantias)
 
