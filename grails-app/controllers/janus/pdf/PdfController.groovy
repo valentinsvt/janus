33a375615151ba269f54bac9e90afcf49f48c9a7
guido8a <guido8a@gmail.com>
2020-03-31 16:02:02 -0500

    Imprimir pdfs desde la web con renderer

--- grails-app/controllers/janus/pdf/PdfController.groovy
+++ grails-app/controllers/janus/pdf/PdfController.groovy
@@ -1,6 +1,9 @@
 package janus.pdf
 
+import janus.seguridad.Shield
 
+
+//class PdfController extends Shield {
 class PdfController {
 
     PdfService pdfService
@@ -8,20 +11,22 @@ class PdfController {
     def index = { redirect(action: demo) }
 
     def pdfLink = {
-        try{
+        try {
             byte[] b
             def baseUri = request.scheme + "://" + request.serverName + ":" + request.serverPort
 
-            params.url = params.url.replaceAll("W","&")
-            if(params.pdfController){
-                def content = g.include(controller:params.pdfController, action:params.pdfAction, id:params.pdfId)
+            params.url = params.url.replaceAll("W", "&")
+
+            if (params.pdfController) {
+                def content = g.include(controller: params.pdfController, action: params.pdfAction, id: params.pdfId)
                 b = pdfService.buildPdfFromString(content.readAsString(), baseUri)
-            }
-            else{
-//                println "sin plugin --> params url "+params.url
+            } else {
+                println "sin plugin --> params url " + params.url
                 def url = baseUri + params.url
-//                println "url pdf "+url
-                b = pdfService.buildPdf(url)
+                println "URL --> " + url
+
+                b = pdfService.buildPdf(url, baseUri)
+//                b = pdfService.buildPdf(params.url, baseUri)
             }
             response.setContentType("application/pdf")
             response.setHeader("Content-disposition", "attachment; filename=" + (params.filename ?: "document.pdf"))
@@ -30,35 +35,31 @@ class PdfController {
         }
         catch (Throwable e) {
             println "there was a problem with PDF generation 2 ${e}"
-            //if(params.template) render(template:params.template)
-            if(params.pdfController){
-//                println "no"
-                redirect(controller:params.pdfController, action:params.pdfAction, params:params)
-            }else{
-                redirect(action: "index",controller: "reportes",params: [msn:"Hubo un error en la genración del reporte. Si este error vuelve a ocurrir comuniquelo al administrador del sistema."])
+            e.printStackTrace()
+            if (params.pdfController) {
+                redirect(controller: params.pdfController, action: params.pdfAction, params: params)
+            } else {
+                redirect(action: "index", controller: "reportes", params: [msn: "Hubo un error en la genración del reporte. Si este error vuelve a ocurrir comuníquelo al administrador del sistema."])
             }
         }
     }
 
     def pdfForm = {
-        try{
+        try {
             byte[] b
             def baseUri = request.scheme + "://" + request.serverName + ":" + request.serverPort + grailsAttributes.getApplicationUri(request)
-            // def baseUri = g.createLink(uri:"/", absolute:"true").toString()
-            if(request.method == "GET") {
+            if (request.method == "GET") {
                 def url = baseUri + params.url + '?' + request.getQueryString()
                 //println "BaseUri is $baseUri"
                 //println "Fetching url $url"
                 b = pdfService.buildPdf(url)
             }
-            if(request.method == "POST"){
+            if (request.method == "POST") {
                 def content
-                if(params.template){
-                    //println "Template: $params.template"
-                    content = g.render(template:params.template, model:[pdf:params])
-                }
-                else{
-                    content = g.include(controller:params.pdfController, action:params.pdfAction, id:params.id, pdf:params)
+                if (params.template) {
+                    content = g.render(template: params.template, model: [pdf: params])
+                } else {
+                    content = g.include(controller: params.pdfController, action: params.pdfAction, id: params.id, pdf: params)
                 }
                 b = pdfService.buildPdfFromString(content.readAsString(), baseUri)
             }
@@ -69,9 +70,9 @@ class PdfController {
         }
         catch (Throwable e) {
             println "there was a problem with PDF generation ${e}"
-            if(params.template) render(template:params.template)
-            if(params.url) redirect(uri:params.url + '?' + request.getQueryString())
-            else redirect(controller:params.pdfController, action:params.pdfAction, params:params)
+            if (params.template) render(template: params.template)
+            if (params.url) redirect(uri: params.url + '?' + request.getQueryString())
+            else redirect(controller: params.pdfController, action: params.pdfAction, params: params)
         }
     }
 
@@ -79,7 +80,7 @@ class PdfController {
         def firstName = params.first ?: "Eric"
         def lastName = params.last ?: "Cartman"
         def age = params.age
-        return [firstName:firstName, lastName:lastName, age:age]
+        return [firstName: firstName, lastName: lastName, age: age]
     }
 
     def demo2 = {
@@ -89,21 +90,21 @@ class PdfController {
         def randomString = params.randomString ?: "PDF creation is a blast!!!"
         def food = params.food
         def hometown = params.hometown
-        return [id:id, name:name, age:age, randomString:randomString, food:food, hometown:hometown]
+        return [id: id, name: name, age: age, randomString: randomString, food: food, hometown: hometown]
     }
 
     def demo3 = {
         def today = new Date()
-        def tomorrow = today +1
-        def content = g.include(controller:"pdf", action:"sampleInclude", params:['today':today, 'tomorrow':tomorrow])
-        return ['content':content, 'pdf':params, 'id':params.id]
+        def tomorrow = today + 1
+        def content = g.include(controller: "pdf", action: "sampleInclude", params: ['today': today, 'tomorrow': tomorrow])
+        return ['content': content, 'pdf': params, 'id': params.id]
     }
 
     def sampleInclude = {
         def bar = 'foo'
         def today = params?.today
         def tomorrow = params?.tomorrow
-        return ['bar':bar, 'today':today, 'tomorrow':tomorrow]
+        return ['bar': bar, 'today': today, 'tomorrow': tomorrow]
         //[today:today, tomorrow:tomorrow]
     }
 
--- grails-app/services/janus/pdf/PdfService.groovy
+++ grails-app/services/janus/pdf/PdfService.groovy
@@ -1,18 +1,93 @@
 package janus.pdf
 
+import com.lowagie.text.FontFactory
+import com.lowagie.text.pdf.BaseFont
+import org.xhtmlrenderer.pdf.ITextFontResolver
 import org.xhtmlrenderer.pdf.ITextRenderer
 
+//import com.lowagie.text.pdf.BaseFont
+//import org.xhtmlrenderer.pdf.ITextFontResolver
+//import org.xhtmlrenderer.pdf.ITextRenderer
+
+/**
+ * Servicio para hacer PDFs
+ */
 class PdfService {
 
     boolean transactional = false
+    def g = new org.codehaus.groovy.grails.plugins.web.taglib.ApplicationTagLib()
 
 /*  A Simple fetcher to turn a specific URL into a PDF.  */
-
-    byte[] buildPdf(url) {
+    /**
+     * Transforma un URL a PDF
+     * @param url
+     * @param pathFonts
+     * @return
+     */
+    byte[] buildPdf(url, String pathFonts) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ITextRenderer renderer = new ITextRenderer();
+
+        FontFactory.registerDirectories();
+
+
+
+        def pf = pathFonts + "${g.resource(dir: 'fonts/PT/PT_Sans')}/"
+        def font = pf + "PT_Sans-Web-Regular.ttf"
+
+        def pf_narrow = pathFonts + "${g.resource(dir: 'fonts/PT/PT_Sans_Narrow')}/"
+        def font_narrow = pf_narrow + "PT_Sans-Narrow-Web-Regular.ttf"
+
+        def pf_bold = pathFonts + "${g.resource(dir: 'fonts/PT/PT_Sans')}/"
+        def font_bold = pf_bold + "PT_Sans-Web-Bold.ttf"
+
+        def pf_narrow_bold = pathFonts + "${g.resource(dir: 'fonts/PT/PT_Sans_Narrow')}/"
+        def font_narrow_bold = pf_narrow_bold + "PT_Sans-Narrow-Web-Bold.ttf"
+
+//        FontResolver resolver = renderer.getFontResolver();
+        renderer.getFontResolver().addFontDirectory(pf, true);
+        renderer.getFontResolver().addFont(font, true);
+        renderer.getFontResolver().addFontDirectory(pf_narrow, true);
+        renderer.getFontResolver().addFont(font_narrow, true);
+        renderer.getFontResolver().addFontDirectory(pf_bold, true);
+        renderer.getFontResolver().addFont(font_bold, true);
+        renderer.getFontResolver().addFontDirectory(pf_narrow_bold, true);
+        renderer.getFontResolver().addFont(font_narrow_bold, true);
+
+//
+        ITextFontResolver fontResolver = renderer.getFontResolver();
+        fontResolver.addFontDirectory(pf, true);
+        fontResolver.addFont(font, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+        fontResolver.addFontDirectory(pf_narrow, true);
+        fontResolver.addFont(font_narrow, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+        fontResolver.addFontDirectory(pf_bold, true);
+        fontResolver.addFont(font_bold, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+//
+//        println "FONT: " + font
+
+/*
+        ITextFontResolver fontResolver = renderer.getFontResolver();
+        fontResolver.addFontDirectory(pathFonts, true);
+
+//        println pathFonts + "arial.ttf"
+
+        fontResolver.addFont(pathFonts + "arial.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+        fontResolver.addFont(pathFonts + "arialbd.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+        fontResolver.addFont(pathFonts + "arialbi.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+        fontResolver.addFont(pathFonts + "ariali.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+        fontResolver.addFont(pathFonts + "ARIALN.TTF", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+        fontResolver.addFont(pathFonts + "ARIALNB.TTF", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+        fontResolver.addFont(pathFonts + "ARIALNBI.TTF", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+        fontResolver.addFont(pathFonts + "ARIALNI.TTF", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+        fontResolver.addFont(pathFonts + "ARIALUNI.TTF", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+        fontResolver.addFont(pathFonts + "ariblk.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+*/
+
+
+//        println "123123123 " + url
         try {
-            renderer.setDocument(url);
+
+            renderer.setDocument(url)
             renderer.layout();
             renderer.createPDF(baos);
             renderer.finishPDF();
@@ -20,7 +95,8 @@ class PdfService {
             return b
         }
         catch (Throwable e) {
-            println "error pdf service " + e
+            e.printStackTrace()
+            log.error e
         }
     }
 
@@ -29,9 +105,18 @@ class PdfService {
   The baseUri is included to allow for relative URL's in the XHTML string
 */
 
+    /**
+     * Transforma una cadena XHTML a PDF
+     * @param content
+     * @param baseUri
+     * @return
+     */
     byte[] buildPdfFromString(content, baseUri) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ITextRenderer renderer = new ITextRenderer();
+//        ITextFontResolver fontResolver = renderer.getFontResolver();
+//        fontResolver.addFont("", true);
+        println "ASDFASDFASDFASDF " + baseUri
         try {
             renderer.setDocumentFromString(content, baseUri);
             renderer.layout();
@@ -43,6 +128,7 @@ class PdfService {
             log.error e
         }
     }
+
 
 }
 
